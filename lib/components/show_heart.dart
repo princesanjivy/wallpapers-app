9f24f61ecb3cb7c85cdf2d8dfe3880e9ff0d3f54
PrinceSanjivy <sanjivy.android@gmail.com>
2021-03-06 23:15:20 +0530

    added lottie

---
+++ lib/components/show_heart.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class HeartAnimation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:hidden_drawer_menu/hidden_drawer_menu.dart';
-import 'package:wallpaper_manager/wallpaper_manager.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -12,6 +10,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.pink,
       body: SimpleHiddenDrawer(
         contentCornerRadius: 30,
         slidePercent: 60,
@@ -41,8 +40,8 @@ class _HomeState extends State<Home> {
           return Scaffold(
             extendBodyBehindAppBar: true,
             appBar: AppBar(
-              elevation: 0,
-              backgroundColor: Colors.transparent,
+              // elevation: 0,
+              // backgroundColor: Colors.transparent,
               leading: InkWell(
                 child: Icon(
                   Icons.menu,
@@ -56,9 +55,14 @@ class _HomeState extends State<Home> {
             body: PageView(
               controller: PageController(),
               children: [
-                Image.network(
-                  "https://images.unsplash.com/photo-1614886750264-afed539cf2bb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
-                  fit: BoxFit.cover,
+                GestureDetector(
+                  onDoubleTap: () {
+                    print("Liked");
+                  },
+                  child: Image.network(
+                    "https://images.unsplash.com/photo-1614886750264-afed539cf2bb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+                    fit: BoxFit.cover,
+                  ),
                 ),
                 Image.network(
                   "https://images.unsplash.com/photo-1614945083613-7763b4897841?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
@@ -69,22 +73,6 @@ class _HomeState extends State<Home> {
                   fit: BoxFit.cover,
                 ),
               ],
-            ),
-            floatingActionButton: FloatingActionButton(
-              child: Icon(
-                Icons.file_download,
-                color: Colors.white,
-              ),
-              onPressed: () async {
-                int location = WallpaperManager.HOME_SCREEN;
-                var file = await DefaultCacheManager().getSingleFile(
-                    "https://images.unsplash.com/photo-1614788404413-ca65466f81f8?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60");
-
-                final String result =
-                    await WallpaperManager.setWallpaperFromFile(
-                        file.path, location);
-                print(result);
-              },
             ),
           );
         },
