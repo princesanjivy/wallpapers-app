ade07285c1546460487a346ad247d2fbaf601d41
PrinceSanjivy <sanjivy.android@gmail.com>
2021-03-08 23:12:20 +0530

    heart animation component

--- lib/components/show_heart.dart
+++ lib/components/show_heart.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
-class HeartAnimation extends StatelessWidget {
+class HeartAnimation extends StatefulWidget {
+  @override
+  _HeartAnimationState createState() => _HeartAnimationState();
+}
+
+class _HeartAnimationState extends State<HeartAnimation> {
   @override
   Widget build(BuildContext context) {
     return Lottie.asset("assets/lottiefiles/hearts.json");
